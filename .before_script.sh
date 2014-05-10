4ea2ffa8b6658631bb9111ad78a13f0aa665c177
brownman <brownman556@gmail.com>
2014-05-10 17:21:50 +0300

    [rm files related to another module]

--- .before_script.sh
+++
@@ -1,8 +0,0 @@
-echo install dependencies 
-sudo apt-get install -y `cat gmail_group.sh | grep depend -m1 | sed 's/#depend://g'`
- exit
-echo --------
-
-cat .travis.yml
-echo -----------
-bash -c ./.present.sh
